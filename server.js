d2d11ead58a1e11a8b454d50e1063953f4c6e273
Laurent Millan <laurent.millan@gmail.com>
2018-03-28 13:41:09 +0200

    Allow CORS

--- server.js
+++ server.js
@@ -100,6 +100,11 @@ const authChecker = function(req, res, next){
 }
 
 app.use(authChecker);
+app.use(function(req, res, next){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 /***************
